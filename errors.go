8cefbddd27208f8d5c833d7951d0f1551087f0c0
Gordan Grasarevic <me@ggordan.com>
2015-03-11 00:03:12 +0000

    Update Error struct to allow recursive inner errors.

--- errors.go
+++ errors.go
@@ -3,16 +3,17 @@ package onedrive
 // Error defines the basic structure of errors that are returned from the OneDrive
 // API.
 // See: http://onedrive.github.io/misc/errors.htm
+
+type InnerError struct {
+	Code       string      `json:"code"`
+	Message    string      `json:"message"`
+	InnerError *InnerError `json:"innererror"`
+}
+
 type Error struct {
-	Body struct {
-		Code       string `json:"code"`
-		Message    string `json:"message"`
-		InnerError struct {
-			Code string `json:"code"`
-		} `json:"innererror"`
-	} `json:"error"`
+	InnerError `json:"error"`
 }
 
 func (e Error) Error() string {
-	return e.Body.Message
+	return e.Message
 }
